87eec232068549d46bee9708b3768f45980c2ca1
Aaron-Ku06 <aaronku320@gmail.com>
2024-01-10 09:23:44 -0500

    Base

---
+++ create-task/main.js
@@ -0,0 +1,33 @@
+import './style.css'
+const helmets = [
+  {
+    Name: "Star Wars Mandalorian Helmet",
+    Image: "",
+    Type: "",
+  },
+  {
+    Name: "Mark 85 Iron Man Helmet",
+    Image: "",
+    Type: "",
+  },
+  {
+    Name: "Red Hood Helmet",
+    Image: "",
+    Type: "",
+  },
+  {
+    Name: "Halo Infinite MKVII Helmet",
+    Image: "",
+    Type: "",
+  },
+  {
+    Name: "Mark 6 War Machine Helmet",
+    Image: "",
+    Type: "",
+  },
+  {
+    Name: "Halo Infinte Master Chief Helmet",
+    Image: "",
+    Type: "",
+  }
+]
